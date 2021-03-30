0237b7bc0b2939f70b9dd6599c3d45371c4706d6
Shivaprasad Bhat <svbhat.neelmane@gmail.com>
2021-03-30 20:03:27 +0530

    Adding Room Database and onther changes
    [ ] Created a AppModule.kt file for dependency injection
    [ ] Added Callback in AppModule.kt level
    [ ] Created a Callback in TaskDatabase.kt

--- app/src/main/java/com/svbneelmane/mvvmtodo/data/TaskDao.kt
+++ app/src/main/java/com/svbneelmane/mvvmtodo/data/TaskDao.kt
@@ -1,8 +1,21 @@
 package com.svbneelmane.mvvmtodo.data
 
-import androidx.room.Dao
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TaskDao {
+
+    // Flow - asynchronous flow of data
+    @Query("SELECT * FROM task_table")
+    fun getTasks(): Flow<List<Task>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(task: Task)
+
+    @Update
+    suspend fun update(task: Task)
+
+    @Delete
+    suspend fun detele(task: Task)
 }
---
+++ app/src/main/java/com/svbneelmane/mvvmtodo/data/TaskDatabase.kt
@@ -0,0 +1,34 @@
+package com.svbneelmane.mvvmtodo.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+import javax.inject.Provider
+
+@Database(entities = [Task::class], version = 1)
+abstract class TaskDatabase : RoomDatabase() {
+    abstract fun taskDao(): TaskDao
+
+    class Callback @Inject constructor(
+        private val database: Provider<TaskDatabase>,
+        private val applicationScope: CoroutineScope
+    ) : RoomDatabase.Callback() {
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+
+            val dao = database.get().taskDao()
+
+            applicationScope.launch {
+                dao.insert(Task("Wash Dishes"))
+                dao.insert(Task("Call wife"))
+                dao.insert(Task("Get ready to party"))
+                dao.insert(Task("Book an appointment with doctor"))
+
+            }
+
+        }
+    }
+}
