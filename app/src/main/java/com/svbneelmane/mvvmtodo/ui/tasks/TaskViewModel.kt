902d2186cb16b38d1ce4c2b019d459d483f7aff2
Shivaprasad Bhat <svbhat.neelmane@gmail.com>
2021-03-31 14:39:25 +0530

    Model 7 : Sorting and Hiding Completed feature implemented.
    Author: Shivaprasad Bhat

--- app/src/main/java/com/svbneelmane/mvvmtodo/ui/tasks/TaskViewModel.kt
+++ app/src/main/java/com/svbneelmane/mvvmtodo/ui/tasks/TaskViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.svbneelmane.mvvmtodo.data.TaskDao
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
 
 class TaskViewModel @ViewModelInject constructor(
@@ -12,9 +13,24 @@ class TaskViewModel @ViewModelInject constructor(
 ) : ViewModel() {
     val searchQuery = MutableStateFlow("")
 
-    private val taskFlow = searchQuery.flatMapLatest {
-        taskDao.getTasks(it)
+    val sortOrder = MutableStateFlow(SortOrder.BY_DATE)
+    val hideCompleted = MutableStateFlow(false)
+
+    private val taskFlow = combine(
+        searchQuery,
+        sortOrder,
+        hideCompleted
+    ) { query, sortOrder, hideCompleted ->
+        Triple(query, sortOrder, hideCompleted)
+
+    }.flatMapLatest {(query, sortOrer, hideCompleted) ->
+        taskDao.getTasks(query, sortOrer, hideCompleted)
     }
 
     val tasks = taskFlow.asLiveData()
+}
+
+
+enum class SortOrder {
+    BY_NAME, BY_DATE
 }
